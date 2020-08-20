5c2bead886dc61b23f701845b9250ed54cb756c7
ipaullly <ipaully22@gmail.com>
2020-08-20 18:45:03 +0300

    implement header and textarea styling as well as fonts

--- src/components/home.page.jsx
+++ src/components/home.page.jsx
@@ -6,36 +6,87 @@ import { HiOutlineTrash } from 'react-icons/hi';
 import './home.styles.scss';
 
 const HomePage = () => {
-  const [adjustedFontSize, setAdjustedFontSize] = useState(1.3)
+  const [adjustedFontSize, setAdjustedFontSize] = useState(1.3);
+  const [checked, setChecked] = useState(false);
+  const [swahiliText, setSwahiliText] = useState('');
+
+  const handleReduceFont = () => {
+    if (adjustedFontSize >= 0.9) {
+      setAdjustedFontSize(adjustedFontSize - 0.3);
+    }
+  }
+
+  const handleIncreaseFont = () => {
+    if (adjustedFontSize <= 2.1) {
+      setAdjustedFontSize(adjustedFontSize + 0.3);
+    }
+  }
+
+  const handleTextChange = (e) => {
+    setSwahiliText(e.target.value)
+  }
+
+  const clearTextField = () => {
+    console.log('click');
+    setSwahiliText('');
+  }
+
   return (
     <div className="container">
       <div className="back-fade" />
       <div className="home">
         <div className="adjust-text">
-          <label class="switch">
-            <input type="checkbox"/>
-            <span class="slider round"></span>
-          </label>
+          <div className="switch-container">
+            <label className="switch">
+              <input
+                onChange={() => setChecked(!checked)}
+                checked={checked}
+                type="checkbox"
+              />
+              <span className="slider round"></span>
+            </label>
+            <span
+              style={{
+                fontFamily: 'Banger',
+                margin: '0 0 0 1vw',
+                letterSpacing: '2px',
+              }}
+            >
+              Banger Font
+            </span>
+          </div>
           <div className="font-adjust-icons">
-            <div className="reduce-font">
-            <BiFont />
-            <AiOutlineMinusCircle />
+            <div className="reduce-font"
+              onClick={() => handleReduceFont()}
+            >
+              <BiFont />
+              <AiOutlineMinusCircle />
             </div>
-            <div className="increase-font">
-            <BiFont />
-            <AiOutlinePlusCircle />
+            <div className="increase-font"
+              onClick={() => handleIncreaseFont()}
+            >
+              <BiFont />
+              <AiOutlinePlusCircle />
             </div>
-            <div className="trash">
+            <div className="trash"
+              onClick={() => clearTextField()}
+            >
               <HiOutlineTrash />
             </div>
           </div>
         </div>
         <div className="text-box">
-          <textarea 
-          style={{
-            fontSize: `${adjustedFontSize}em`
-          }}
-          name="text" placeholder="Type here..." />
+          <textarea
+            style={{
+              letterSpacing: '2px',
+              fontSize: `${adjustedFontSize}em`,
+              fontFamily: checked ? 'Banger' : 'sans-serif',
+            }}
+            name="text"
+            placeholder='Type Here . . .'
+            value={swahiliText}
+            onChange={(e) => handleTextChange(e)}
+          />
           <br />
           <br />
         </div>
--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,39 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { ImPlay3 } from 'react-icons/im';
+import { BsDownload } from 'react-icons/bs';
 
 import './navbar.styles.scss'
 
 const Navbar = () => {
+  const [voice, setVoice] = useState('swahili');
+
   return (
     <div className='navbar'>
-      <img 
-        src="https://i.ibb.co/yBVYbfN/Slide2.png"
-        alt="Slide2"
-        border="0"
-        className='logo'
-      />
-        <div class="navbar-brand-title">Bonga kaWeWe</div>
-        <a href="/" class="toggle-button">
-            <span class="bar" style={{
-              backgroundColor: 'black'
-            }}>home</span>
-            <span class="bar" style={{
-              backgroundColor: 'black'
-            }}>home</span>
-            <span class="bar" style={{
-              backgroundColor: 'black'
-            }}>home</span>
-        </a>
-
-        <div class="navbar-links">
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#voices">Voices</a></li>
-                <li><a href="#blog">Blog</a></li>
-                <li><a href="#faq">Faq</a></li>
-            </ul>
+      <div className="logo-container">
+        <img 
+          src="https://i.ibb.co/yBVYbfN/Slide2.png"
+          alt="Slide2"
+          border="0"
+          className='logo'
+        />
+        <div className="navbar-brand-title">Bonga kaWeWe</div>
+      </div>
+      <div className="filters-container">
+        <div className="stream-button"><ImPlay3 /></div>
+        <div className="download-button"><BsDownload /><span>MP3</span></div>
+        <div className="voice-dropdown">
+          <button class="dropbtn">Voices</button>
+          <div class="dropdown-content">
+            <span
+              onClick={() => setVoice('english')}
+            >English</span>
+            <span
+              onClick={() => setVoice('italian')}
+            >Italian</span>
+            <span
+              onClick={() => setVoice('swahili')}
+            >Swahili</span>
+          </div>
         </div>
+        <div className="toggle-speech-speed">Toggle speed</div>
+      </div>
+      <div className="navbar-links">
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
